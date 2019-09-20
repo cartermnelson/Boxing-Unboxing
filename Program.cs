fee1dda19bb43f47b7df76a59c9af63874cdc6f3
Carter Nelson <carternelson05@gmail.com>
2019-09-20 10:33:04 -0700

    Created list

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Boxing_Unboxing
 {
@@ -6,7 +7,12 @@ namespace Boxing_Unboxing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<object> list = new List<object>();
+            list.Add(7);
+            list.Add(28);
+            list.Add(-1);
+            list.Add(true);
+            list.Add("chair");
         }
     }
 }
